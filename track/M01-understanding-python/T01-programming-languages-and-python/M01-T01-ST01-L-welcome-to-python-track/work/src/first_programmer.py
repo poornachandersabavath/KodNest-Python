3ee75b2e4b722d82ecaa9168420edc922f01a29c
poornachandersabavath <poornacahndarpmv@gmail.com>
2026-09-20 16:35:27 +0530

    day 7 fundanction

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_programmer.py
@@ -0,0 +1,14 @@
+print("This is my first program")
+
+#write a python program that prints the following information:
+print("My name is poornachanader")
+print("I am 22 years old")
+print("I am form hyderabad")
+print("My favourite programming language is python")
+print("I start my python journey with kodnest within one week i have really enjoyed exploring the fundamentals")
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/pseudo.py
@@ -0,0 +1,67 @@
+#START
+#print "hello world"
+#END
+print("hello world")
+
+print("Hello World \nThank for learning python")
+
+print("Hello World",end="-")
+print("Thank for learning python")
+
+name="Gamana"
+print("name:\t",name)
+
+# To find number is even or add
+# Start
+# input n
+# if n%2 ==0
+# print="even"
+# else
+# print="odd"
+# END
+n=11
+if n%2 ==0:
+    print("even")
+else:
+    print("odd")
+
+# find the larest among the 3 numbers a,b,c
+# start
+# input a
+# input b
+# input c
+# if  a>=b and a>=c
+# print="a is largest"
+# else if b>=a and b>=c
+# print="b is largest"
+# else 
+# print="c is largest"
+# END
+a=40
+b=20
+c=30
+if a>=b and a>=c:
+    print("a is largest")
+elif b>=a and b>=c:
+    print("b is largest")
+else:
+    print("c is largest")
+
+'''
+start
+input n
+if n>0
+print("positive")
+else if n<0
+print("negative")
+else
+print("zero")
+end
+'''
+n=14
+if n>0:
+    print("positive")
+elif n<0:
+    print("negative")
+else:
+    print("zero")
