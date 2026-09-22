26ae97dfa134592e3d9e944188fb99d9ad465635
poornachandersabavath <poornacahndarpmv@gmail.com>
2026-09-22 23:44:29 +0530

    written the datatype programming

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/datatypes.py
@@ -0,0 +1,16 @@
+age=10
+print(age,type(age))
+
+marks=99.9
+print(marks,type(marks))
+
+name="amma"
+print(name,type(name))
+
+
+csum=3+5j
+print(csum,type(csum))
+
+has_job=False
+print(has_job,type(has_job))
+
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/variables.py
@@ -0,0 +1,37 @@
+name="poornachander"
+name="gamana"
+print(name)
+
+age=22
+#my name is poornachander and age is 22
+print("my name is"+" "+ name +" "+ "and age is"+" ",age)
+print("my name is "+ name + " and age is "+ str(age))
+
+# formatting string
+print(f"my name is {name} and age is {age}")
+
+a=10
+b=20
+c=30
+print(a,b,c)
+
+x,y,z=100,200,300
+print(x,y,z)
+
+x=y=z=200
+print(x,y,z)
+
+print(x*a)
+print(x+b)
+
+
+
+
+
+
+
+
+
+
+
+
